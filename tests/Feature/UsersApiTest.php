63ce9e031c33f6349079afba2e92f3892b64b933
William Edmunds <wwedmunds@gmail.com>
2019-07-07 09:02:08 +0100

    registration & login functionality endpoint tests

--- tests/Feature/UsersApiTest.php
+++ tests/Feature/UsersApiTest.php
@@ -7,22 +7,88 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UsersApiTest extends TestCase
 {
+
     /**
      * Registration endpoint test
-     *
-     * @return void
      */
     public function testUserCanRegisterOk(){
+
+        $testingId = uniqid();
+
         $response = $this->post('/api/register', [
-            'email' => uniqid().'@gmail.com',
+            'email' => $testingId.'@gmail.com',
             'name' => 'testing',
-            'password' => uniqid()
+            'password' => $testingId
         ]);
         $response->assertStatus(200)->assertJson(
             [
                 'status' => 'success',
             ]
         );
+
+        return $testingId;
             
+    }
+
+    /**
+     * Login and key assignment functionality
+     * 
+     * @depends testUserCanRegisterOk
+     */
+    public function testUserCanLoginOkAndGetsToken($testingId) {
+
+        $response = $this->post('/api/login', [
+            'email' => $testingId.'@gmail.com',
+            'password' => $testingId
+        ]);
+
+        $response->assertStatus(200)->assertJson(
+            [
+                'success' => true
+            ]
+        );
+
+        $responseJson = json_decode($response->getContent(), true);
+
+        $this->assertArrayHasKey('token', $responseJson);
+
+    }
+
+    /**
+     * Login functionality fails in the correct manner
+     */
+    public function testUserLoginFailsWithoutEmailOrPassword(){
+
+        $response = $this->post('/api/login', [
+            'password' => 'testing'
+        ]);
+
+        $response->assertStatus(200)->assertJson(
+            [
+                'success' => false
+            ]
+        );
+
+
+        $response = $this->post('/api/login', [
+            'email' => 'testing@gmail.com',
+        ]);
+
+        $response->assertStatus(200)->assertJson(
+            [
+                'success' => false
+            ]
+        );
+
+        
+        $response = $this->post('/api/login', [
+        ]);
+
+        $response->assertStatus(200)->assertJson(
+            [
+                'success' => false
+            ]
+        );
+
     }
 }
